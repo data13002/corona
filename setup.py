5e3c2ff02795065387c83b782e043c6a70a414a6
GILLES Thea <thea.gilles@etu.univ-amu.fr>
2020-03-06 12:08:37 +0100

    initial commiinitial commiinitial committt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='On va vous sauver du corona',
+    author='gilles',
+    license='MIT',
+)
